649ebfa2ce52f8ca261b090a1edf6d63361c8d18
mohamedelkhateeb <mohamedelkhteeb74@gmail.com>
2023-10-24 01:05:58 +0300

    10/24/2023-1:05am

--- FirstApp.API/Controllers/VillaApiController.cs
+++ FirstApp.API/Controllers/VillaApiController.cs
@@ -7,18 +7,109 @@ using Microsoft.AspNetCore.Mvc;
 namespace FirstApp.API.Controllers
 {
     [Route("api/VillaApi")]
-    [ApiController]
+    //[ApiController]
     public class VillaApiController : ControllerBase
     {
+        //1-Get All
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult <IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaDTOs;
+            //For Status code
+            return Ok(VillaStore.villaDTOs);
         }
+        //2-Get By Id
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("id")]
-        public VillaDTO GetVilla(int id )
+        public ActionResult <VillaDTO> GetVilla(int id )
         {
-            return VillaStore.villaDTOs.FirstOrDefault(v => v.Id == id);
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var result = VillaStore.villaDTOs.FirstOrDefault(v => v.Id == id);
+            if(result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+        //3-Create (post) Item
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPost("AddVilla")]
+        public ActionResult <VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
+        {
+            //Custom Validation
+            //if(!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+
+            //Check if The name is exist
+            if(VillaStore.villaDTOs.FirstOrDefault(x => x.Name.ToLower() == 
+            villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("", "Villa Name Already Exists!");
+                return BadRequest(ModelState);
+            }
+
+
+            if(villaDTO == null)
+            {
+                return BadRequest(villaDTO);
+            }
+
+            if(villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            villaDTO.Id = VillaStore.villaDTOs.OrderByDescending(i => i.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaDTOs.Add(villaDTO);
+            return Ok(villaDTO);
+        }
+        //Delete Item
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpDelete("id")]
+        public IActionResult DeleteVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var result = VillaStore.villaDTOs.FirstOrDefault(v => v.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            VillaStore.villaDTOs.Remove(result);
+
+            return NoContent();
+        }
+        //3-Create (post) Item
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPut("UpdateVilla")]
+        public IActionResult Updateilla(int id , [FromBody] VillaDTO villaDTO)
+        {
+            if (villaDTO == null || id != villaDTO.Id)
+            {
+                return BadRequest(villaDTO);
+            }
+
+            var result = VillaStore.villaDTOs.FirstOrDefault(v => v.Id == id);
+            result.Name = villaDTO.Name;
+            return NoContent();
         }
     }
 }
--- FirstApp.API/Models/DTO/VillaDTO.cs
+++ FirstApp.API/Models/DTO/VillaDTO.cs
@@ -1,9 +1,12 @@
-﻿namespace FirstApp.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstApp.API.Models.DTO
 {
     public class VillaDTO
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
-        public DateTime CreationDate { get; set; }
     }
 }
